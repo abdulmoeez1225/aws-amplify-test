ea1d502565d667125372961c2735564da84b1e91
abdulmoeez1225 <abdulmoeez1225@gmail.com>
2023-05-24 21:50:15 +0500

    first commit

---
+++ packages/core/__tests__/MemoryStorage-test.ts
@@ -0,0 +1,20 @@
+import { MemoryStorage, StorageHelper } from '../src/StorageHelper';
+
+describe('StorageHelper', () => {
+	test('MemoryStorage', () => {
+		MemoryStorage.setItem('k', 'v');
+		expect(MemoryStorage.getItem('k')).toEqual('v');
+		MemoryStorage.removeItem('k');
+		expect(MemoryStorage.getItem('k')).toBeFalsy();
+		MemoryStorage.setItem('k', 'v');
+		MemoryStorage.clear();
+		expect(MemoryStorage.getItem('k')).toBeFalsy();
+	});
+
+	test('StorageHelper', () => {
+		const helperA = new StorageHelper().getStorage();
+		expect(helperA instanceof Storage).toBeTruthy();
+		const helperB = new StorageHelper().getStorage();
+		expect(helperB instanceof Storage).toBeTruthy();
+	});
+});
